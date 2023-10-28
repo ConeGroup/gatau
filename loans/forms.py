53088d72d49b4920578ca34207b59f3130be3e1a
nurinfarzana25 <nurinfarzana25@gmail.com>
2023-10-29 00:04:54 +0700

    yoo

--- loans/forms.py
+++ loans/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from loans.models import LoansBook
 
-class LoanForm(ModelForm):
+class LoanForm(forms.ModelForm):
     class Meta:
         model = LoansBook
         fields = ["number_book", "date_return"]
+
+        widgets = {
+            'number_book': forms.NumberInput(attrs={'class': 'form-control'}),
+            'date_return': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min': '{{ today }}', 'max': '{{ maxDate }}'}),
+        }
