d768d8862a8e4ab6eb7d9ed39a42adbf15b5bf29
nandapratama21 <nanda241power@gmail.com>
2023-10-28 11:51:48 +0700

    Tugas kelompok UTS

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from home.views import home, user_home
 from home.views import logout_user, login_user
 from home.views import register
+from userprofile.views import show_userprofile
+
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register, name='register'),
+    path('userprofile/', show_userprofile, name='userprofile'),
 ]
--- librarium/urls.py
+++ librarium/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('api/books/', include('book.urls')),
+    path('userprofile/', include(('userprofile.urls', 'userprofile'), namespace='userprofile')),
     
 ]
