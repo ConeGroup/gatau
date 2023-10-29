0e1bc68270ae356054ceb023298f0c7b7725e7a4
nurinfarzana25 <nurinfarzana25@gmail.com>
2023-10-29 14:17:39 +0700

    yoo

--- loans/urls.py
+++ loans/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from loans.views import show_loans, add_book_ajax, delete_book, get_product_json, get_book_json, show_loans_page
+from home.views import home, user_home
 
 app_name = 'loans'
 
@@ -9,5 +10,7 @@ urlpatterns = [
     path('delete_book/<int:book_id>', delete_book, name='delete_book'),
     path('get_product_json/', get_product_json, name='get_product_json'),
     path('get_book_json/', get_book_json, name='get_book_json'),
-    path('show_loans_page/', show_loans_page, name='show_loans_page')
+    path('show_loans_page/', show_loans_page, name='show_loans_page'),
+    path('', home, name='home'),
+    path('user-page/', user_home, name='user_home'),
 ]
