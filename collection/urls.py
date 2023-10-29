a6337cef5470776b75ed25c841dc34c13e1327ca
resandadezcaasyam <dezca30@gmail.com>
2023-10-29 18:08:27 +0700

    Bismillah Selesai Modul Collections

--- collection/urls.py
+++ collection/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from collection.views import add_book_to_collection_ajax, collection_list, create_collection_ajax, delete_collection, edit_collection, get_book_json, get_collections_json
+from collection.views import add_book_to_collection, add_book_to_collection_ajax, remove_book_from_collection, show_collection, create_collection_ajax, delete_collection, edit_collection, get_book_json, get_collection_json, show_json, show_json_by_id
 
 app_name = 'collection'
 
 urlpatterns = [
-    path('', collection_list, name='collection_list'),
-    path('add_book_to_collection_ajax/', add_book_to_collection_ajax, name='add_book_to_collection_ajax'),
-    path('create_collection_ajax/', create_collection_ajax, name='create_collection_ajax'),
-    path('edit_collection/<int:collection_id>/', edit_collection, name='edit_collection'),
+    path('', show_collection, name='show_collection'),
+    path('add-book-to-collection-ajax/', add_book_to_collection_ajax, name='add_book_to_collection_ajax'),
+    path('create-collection-ajax/', create_collection_ajax, name='create_collection_ajax'),
+    path('edit-collection/<int:collection_id>/', edit_collection, name='edit_collection'),
     path('delete_collection/<int:collection_id>/', delete_collection, name='delete_collection'),
-    path('get_collections_json/', get_collections_json, name='get_collections_json'),  # Tambahkan ini
-    path('get_book_json/', get_book_json, name='get_book_json'),  # Tambahkan ini
+    path('get-collection-json/', get_collection_json, name='get_collection_json'),  # Tambahkan ini
+    path('get-book-json/', get_book_json, name='get_book_json'),  # Tambahkan ini
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('<int:collection_id>/add-book/', add_book_to_collection, name='add_book_to_collection'),
+    path('<int:collection_id>/remove-book/<int:book_id>/', remove_book_from_collection, name='remove_book_from_collection'),
 ]
