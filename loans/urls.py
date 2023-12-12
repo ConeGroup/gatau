3cb306644d61d31f8d892814ae11c4d8f8ef8be8
nurinfarzana25 <nurinfarzana25@gmail.com>
2023-12-12 20:10:58 +0700

    add function in views.py loans

--- loans/urls.py
+++ loans/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from loans.views import show_loans, add_book_ajax, delete_book, get_product_json, get_book_json, show_loans_page
+from loans.views import show_loans, add_book_ajax, delete_book, get_product_json, get_book_json, show_loans_page, create_loans_flutter
 from home.views import home, user_home
 
 app_name = 'loans'
@@ -13,4 +13,5 @@ urlpatterns = [
     path('show_loans_page/', show_loans_page, name='show_loans_page'),
     path('', home, name='home'),
     path('user-page/', user_home, name='user_home'),
+    path('create-loans-flutter/', create_loans_flutter, name='create_loans_flutter'),
 ]
