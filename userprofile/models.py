b86ebb1b5bdbaf4e0ab0fbdb37ce7c0a70f82874
nandapratama21 <nanda241power@gmail.com>
2023-12-20 20:13:34 +0700

    TAMBAHIN MODEL

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import User
 from home.forms import RegisterForm
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
